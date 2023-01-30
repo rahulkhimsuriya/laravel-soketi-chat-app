475b509d5a99d38812f10f45f0e54353cc1ab84e
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2023-01-30 20:44:08 +0530

    add icon and refactor

--- resources/js/Pages/UserChat/Index.vue
+++ resources/js/Pages/UserChat/Index.vue
@@ -1,58 +1,62 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import { useForm } from '@inertiajs/inertia-vue3'
-import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import DangerButton from '@/Components/DangerButton.vue';
-import { onMounted,reactive,computed } from 'vue';
-import { usePage } from '@inertiajs/inertia-vue3'
-import { Pusher } from '@/pusher'
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { useForm } from "@inertiajs/inertia-vue3";
+import InputError from "@/Components/InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import DangerButton from "@/Components/DangerButton.vue";
+import { onMounted, reactive, computed } from "vue";
+import { usePage } from "@inertiajs/inertia-vue3";
+import { Pusher } from "@/pusher";
 
 const props = defineProps({
     chats: [],
-    sender: null
+    sender: null,
 });
 
-const authUser = computed(() => usePage().props.value.user)
+const authUser = computed(() => usePage().props.value.user);
 
 const state = reactive({
     chats: props.chats,
-})
+});
 
 const form = useForm({
-    message: '',
-})
+    message: "",
+});
 
 const sendMessage = () => {
-    form.post(route('user.chat.store',{ user: props.sender.id }),{
+    form.post(route("user.chat.store", { user: props.sender.id }), {
         preserveScroll: true,
         onSuccess: () => {
-            form.reset()
-            window.location.reload()
+            form.reset();
+            window.location.reload();
         },
-    })
-}
+    });
+};
+
+const removeChatMessageById = (chatId) => {
+    state.chats = state.chats.filter((chat) => chat.id != chatId);
+};
 
 const deleteChat = async (chatId) => {
     const form = useForm({});
-    form.delete(route('chat.destroy',{ chat: chatId }))
+    form.delete(route("chat.destroy", { chat: chatId }));
 
-    state.chats = state.chats.filter(c => c.id != chatId)
-}
+    removeChatMessageById(chatId);
+};
 
 onMounted(() => {
-    const senderId = props.sender.id
-    const receiverId = authUser.value.id
+    const senderId = props.sender.id;
+    const receiverId = authUser.value.id;
 
     Pusher.subscribe(`chat.sender.${senderId}.receiver.${receiverId}`)
-        .bind('new-message',function ({ chat }) {
-            state.chats.push(chat)
+        .bind("new-message", function ({ chat }) {
+            state.chats.push(chat);
         })
-        .bind('chat-message-deleted',function ({ chatId }) {
-            state.chats = state.chats.filter(c => c.id != chatId)
-        })
-})
+        .bind("chat-message-deleted", function ({ chatId }) {
+            removeChatMessageById(chatId);
+        });
+});
 </script>
 
 <template>
@@ -66,10 +70,13 @@ onMounted(() => {
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
                     <template v-if="state.chats.length">
                         <ul class="px-6 py-2">
-                            <li v-for="chat in state.chats" :key="chat.id" class="py-1 flex justify-between">
+                            <li
+                                v-for="chat in state.chats"
+                                :key="chat.id"
+                                class="py-1 flex justify-between"
+                            >
                                 <span>
                                     <strong>{{ chat.sender.name }} : </strong>
                                     <span class="ml-2">
@@ -78,7 +85,9 @@ onMounted(() => {
                                 </span>
 
                                 <span v-if="authUser.id === chat.sender.id">
-                                    <DangerButton @click="deleteChat(chat.id)">X</DangerButton>
+                                    <DangerButton @click="deleteChat(chat.id)"
+                                        >X</DangerButton
+                                    >
                                 </span>
                             </li>
                         </ul>
@@ -88,17 +97,45 @@ onMounted(() => {
                         <form @submit.prevent="sendMessage" class="">
                             <div class="flex align-items-center">
                                 <div class="w-1/2">
-                                    <TextInput id="message" v-model="form.message" type="text" class="mt-1 block w-full"
-                                        autocomplete="message" placeholder="Say something..." />
+                                    <TextInput
+                                        id="message"
+                                        v-model="form.message"
+                                        type="text"
+                                        class="mt-1 block w-full"
+                                        autocomplete="message"
+                                        placeholder="Say something..."
+                                    />
                                 </div>
 
-                                <PrimaryButton class="ml-2 w-32" :class="{ 'opacity-25': form.processing }"
-                                    :disabled="form.processing">
-                                    Send
+                                <PrimaryButton
+                                    class="ml-2 w-32 flex items-center justify-center"
+                                    :class="{ 'opacity-25': form.processing }"
+                                    :disabled="form.processing"
+                                >
+                                    <span> Send </span>
+                                    <span class="ml-1">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="w-5 h-5"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                                            />
+                                        </svg>
+                                    </span>
                                 </PrimaryButton>
                             </div>
 
-                            <InputError :message="form.errors.message" class="mt-2" />
+                            <InputError
+                                :message="form.errors.message"
+                                class="mt-2"
+                            />
                         </form>
                     </div>
                 </div>
